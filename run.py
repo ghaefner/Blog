8ce91996d76c627d35d0cad7b09f1437b715f10a
ghaefner <guillaume-haefner@gmx.de>
2024-02-05 19:43:13 +0100

    Change run for public deployment

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensures that the database tables are created before running the app
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
