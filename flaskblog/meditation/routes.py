2438fc35b8a13332fdb9a4cf0e1a817f31619ce5
ghaefner <guillaume-haefner@gmx.de>
2024-03-02 10:42:03 +0100

    Change routes function for meditation videos

--- flaskblog/meditation/routes.py
+++ flaskblog/meditation/routes.py
@@ -3,5 +3,5 @@ from flask import render_template, Blueprint
 meditation = Blueprint('meditation', __name__, template_folder='templates')
 
 @meditation.route("/meditation/videos")
-def meditation_videos():
+def videos():
     return render_template('meditation_video.html', title='Meditation Videos')
