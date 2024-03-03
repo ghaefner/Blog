44232b8186ec0bb8c3f6a8cfa9710629e84e9c07
ghaefner <guillaume-haefner@gmx.de>
2024-03-03 21:08:38 +0100

    Add routes for calendar

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Blueprint
 from flaskblog.models import Post
+from calendar import TextCalendar
 
 main = Blueprint('main', __name__)
 
@@ -26,3 +27,18 @@ def about():
         render_template: About page template.
     """
     return render_template('about.html', title='About')
+
+@main.route("/calendar")
+def calendar():
+    """
+    Render the clendar page.
+
+    Returns:
+        render_template: Calendar page template.
+    """
+    year = 2024
+    month = 2
+
+    cal = TextCalendar(calendar.SUNDAY)
+    cal_text = cal.formatmonth(year, month)
+    return render_template('calendar.html', calendar_text=cal_text)
