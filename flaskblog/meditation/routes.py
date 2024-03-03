5b3928b66163c0ac2329294f5cf152e14296844d
ghaefner <guillaume-haefner@gmx.de>
2024-03-03 19:04:34 +0100

    Add mindful exercise route function to meditation module

--- flaskblog/meditation/routes.py
+++ flaskblog/meditation/routes.py
@@ -5,3 +5,7 @@ meditation = Blueprint('meditation', __name__, template_folder='templates')
 @meditation.route("/meditation/videos")
 def videos():
     return render_template('meditation_video.html', title='Meditation Videos')
+
+@meditation.route("/meditation/exercise")
+def excercise():
+    return render_template('mindful_exercise.html', title='Mindfulness Exercise')
