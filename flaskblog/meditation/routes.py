8980a0295da74bf6093fe4b771abff5ae780d976
ghaefner <guillaume-haefner@gmx.de>
2024-03-10 16:43:35 +0100

    Add routing for mindful questionaire

--- flaskblog/meditation/routes.py
+++ flaskblog/meditation/routes.py
@@ -9,3 +9,13 @@ def videos():
 @meditation.route("/meditation/exercise")
 def exercise():
     return render_template('mindful_exercise.html', title='Mindfulness Exercise')
+
+@meditation.route('/mindful_question', methods=['GET', 'POST'])
+def mindful_question():
+    # form = MindfulQuestionForm()
+    # if form.validate_on_submit():
+        # Process form data here
+        # For example, you can save the submitted data to the database
+        # Or perform any other actions based on the user's input
+        # Redirect the user to a new page or display a success message
+    return render_template('mindful_question.html', title='Mindfulness Questionnaire') # form=form)
